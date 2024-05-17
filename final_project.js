0ab58f51cc2a2e4c8e0291018073e2709e1f04db
chunb1 <samtw1024@gmail.com>
2024-05-17 12:44:05 -0400

    commit

--- final_project.js
+++ final_project.js
@@ -1,6 +1,34 @@
-const cuisineButton = dsocument.querySelector('.cuisine')
+const cuisineBtn = document.querySelector('.cuisine')
+const cuisineBackground = document.querySelector('.cuisine-options-background')
+const applyBtn = document.querySelector('.apply')
+const cuisineOptions = document.querySelector('.cuisine-options')
 
 
-cuisineButton.addEventListener('click', => {
-    
+
+// cuisineBtn.addeventlistener('click', () => {
+//     cusineBackground.style.display = 'block'
+// })
+
+cuisineBtn.addEventListener('click', () => {
+    if (cuisineBackground.style.display == 'none' && cuisineOptions.style.display == 'none'
+    ){
+        cuisineBackground.style.display = 'block'
+        cuisineOptions.style.display = 'flex'
+    }
+    else{
+        cuisineBackground.style.display = 'none'
+        cuisineOptions.style.display = 'none'
+    }
 })
+
+applyBtn.addEventListener('click', () => {
+    if (cuisineBackground.style.display == 'block' && cuisineOptions.style.display == 'flex'){
+        cuisineBackground.style.display = 'none'
+        cuisineOptions.style.display = 'none'
+    }
+    else{
+        cuisineBackground.style.display = 'block'
+        cuisineOptions.style.display = 'block'
+    }
+})
+
